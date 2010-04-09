76902d77908152e72a9c60b25702c15b11c3ec1e
vmzakhar <vmzakhar@0ab34ceb-dc6f-dbc6-6f38-926ef19db99f>
2010-04-09 01:10:57 +0000

    Slowly moving forward

--- CorpsePack.cpp
+++ CorpsePack.cpp
@@ -37,102 +37,121 @@ using namespace MAUI;
 
 class DrawingContext {
 public:
-	Font* mainFont;
-	Font* boldFont;
-	int backgroundColor;
+    Font* mainFont;
+    Font* boldFont;
+    int backgroundColor;
 
-	DrawingContext(MAHandle resourceID) {
-		int i;
-		int position = 0;
-		maReadData(resourceID, &i, position, sizeof(i)); position += sizeof(i);
-		this->mainFont = new Font(i);
-		maReadData(resourceID, &i, position, sizeof(i)); position += sizeof(i);
-		this->boldFont = new Font(i);
-		maReadData(resourceID, &i, position, sizeof(i)); position += sizeof(i);
-		backgroundColor = i;
-	}
+    DrawingContext(MAHandle resourceID) {
+        int i;
+        int position = 0;
+        maReadData(resourceID, &i, position, sizeof(i)); position += sizeof(i);
+        this->mainFont = new Font(i);
+        maReadData(resourceID, &i, position, sizeof(i)); position += sizeof(i);
+        this->boldFont = new Font(i);
+        maReadData(resourceID, &i, position, sizeof(i)); position += sizeof(i);
+        backgroundColor = i;
+    }
 
-	DrawingContext(MAHandle mainFontResource, MAHandle boldFontResource, int backgroundColor) {
-		this->mainFont = new Font(mainFontResource);
-		this->boldFont = new Font(boldFontResource);
-		this->backgroundColor = backgroundColor;
-	}
+    DrawingContext(MAHandle mainFontResource, MAHandle boldFontResource, int backgroundColor) {
+        this->mainFont = new Font(mainFontResource);
+        this->boldFont = new Font(boldFontResource);
+        this->backgroundColor = backgroundColor;
+    }
 };
 
 class CorpsePackScreen : public Screen {
 private:
-	Widget* main;
-	Label* header;
-	ListBox* content;
-	Label* footer;
-	int y;
+    Widget* main;
+    Label* header;
+    ListBox* content;
+    Layout* footer;
+    Label* leftSoftButton;
+    Label* rightSoftButton;
+	enum States { MAIN, POPUP, EVENT } state;
 
 public:
-	DrawingContext* drawingContext;
+    DrawingContext* drawingContext;
 
-	CorpsePackScreen(DrawingContext* drawingContext) {
-		y = 10;
-		this->drawingContext = drawingContext;
-    	MAExtent screenSize = maGetScrSize();
-    	int screenWidth = EXTENT_X(screenSize);
-    	int screenHeight = EXTENT_Y(screenSize);
-    	Layout* layout = new Layout(0, 0, screenWidth, screenHeight, NULL, 1, 3);
-    	header = new Label(0, 0, screenWidth, 0, NULL, "CorpsePack v0.1  ËÄÇ: ÂÀÐìèÿ", 0xff0000 /*drawingContext->backgroundColor*/, drawingContext->boldFont);
-    	header->setDrawBackground(true);
-    	header->setAutoSizeX(false);
-    	header->setAutoSizeY(true);
-		footer = new Label(0, 0, screenWidth, 0, NULL, "Help      Exit", 0xff0000 /*drawingContext->backgroundColor*/, drawingContext->boldFont);
-		footer->setDrawBackground(true);
-		footer->setAutoSizeX(false);
-		footer->setAutoSizeY(true);
-		content = new ListBox(0, 0, screenWidth, screenHeight - header->getHeight() - footer->getHeight() - 30, NULL, ListBox::LBO_VERTICAL, ListBox::LBA_LINEAR, false);
-		content->setDrawBackground(true);
-		layout->add(header);
-		layout->add(content);
-		layout->add(footer);
-		main = layout;
-		setMain(main);
-	}
+    CorpsePackScreen(DrawingContext* drawingContext) {
+        this->drawingContext = drawingContext;
+        MAExtent screenSize = maGetScrSize();
+        int screenWidth = EXTENT_X(screenSize);
+        int screenHeight = EXTENT_Y(screenSize);
+        Layout* layout = new Layout(0, 0, screenWidth, screenHeight, NULL, 1, 3);
 
-	~CorpsePackScreen() {
-		delete main;
-	}
+        const char* title = "CorpsePack v0.1  ËÄÇ: ÂÀÐìèÿ";
+        int labelHeight = EXTENT_Y(drawingContext->boldFont->getStringDimensions(title)) + 1;
+        header = new Label(0, 0, screenWidth, labelHeight, NULL, title, 0xff0000 /*drawingContext->backgroundColor*/, drawingContext->boldFont);
 
-	void keyPressEvent(int keyCode)	{
-		drawingContext->mainFont->drawString(MAUtil::integerToString(keyCode).c_str(), 0, y += 10);
+        footer = new Layout(0, 0, screenWidth, labelHeight, NULL, 2, 1);
+        leftSoftButton = new Label(0,0, screenWidth / 2, labelHeight, footer, "Help", 0xff0000, drawingContext->boldFont);
+        leftSoftButton->setHorizontalAlignment(Label::HA_LEFT);
+        leftSoftButton->setPaddingLeft(EXTENT_X(drawingContext->boldFont->getStringDimensions("M")));
+        rightSoftButton = new Label(0,0, screenWidth / 2, labelHeight, footer, "Exit", 0xff0000, drawingContext->boldFont);
+        rightSoftButton->setHorizontalAlignment(Label::HA_RIGHT);
+        rightSoftButton->setPaddingRight(EXTENT_X(drawingContext->boldFont->getStringDimensions("M")));
+
+        content = new ListBox(0, 0, screenWidth, screenHeight - header->getHeight() - footer->getHeight(), NULL, ListBox::LBO_VERTICAL, ListBox::LBA_LINEAR, false);
+        content->setBackgroundColor(0xffffff);
+        layout->add(header);
+        layout->add(content);
+        layout->add(footer);
+        main = layout;
+        setMain(main);
+        state = MAIN;
+    }
+
+    ~CorpsePackScreen() {
+        delete main;
+    }
+
+    void keyPressEvent(int keyCode) {
+        switch(keyCode) {
+        case MAK_SOFTLEFT:
+        case MAK_SOFTRIGHT:
+        	if (state == MAIN) {
+        		state = POPUP;
+        	}
+        case MAK_DOWN:
+        	if (state == MAIN) {
+        		content->selectNextItem(true);
+        	}
+            break;
+        case MAK_UP:
+        	if (state == MAIN) {
+        		content->selectPreviousItem(true);
+        	}
+            break;
+        }
         maUpdateScreen();
-    	switch(keyCode) {
-		case MAK_DOWN:
-			content->selectNextItem(true);
-			break;
-		case MAK_UP:
-			content->selectPreviousItem(true);
-			break;
-    	}
-	}
+    }
 
     void keyReleaseEvent(int keyCode) {
-    	drawingContext->mainFont->drawString(MAUtil::integerToString(keyCode).c_str(), 0, y += 10);
+    	if (state == POPUP) {
+    		state = MAIN;
+    	}
         maUpdateScreen();
-    	if (keyCode == MAK_SOFTRIGHT) {
-			maExit(0);
-		}
     }
 };
 
 class CorpsePackMoblet : public Moblet {
-
 private:
-	CorpsePackScreen* screen;
-
+    CorpsePackScreen* screen;
 public:
     CorpsePackMoblet() {
-    	screen = new CorpsePackScreen(new DrawingContext(DRAWING_CONTEXT));
-    	screen->show();
+        screen = new CorpsePackScreen(new DrawingContext(DRAWING_CONTEXT));
+        screen->show();
     }
 
     ~CorpsePackMoblet() {
-    	delete screen;
+        delete screen;
+    }
+
+    void keyReleaseEvent(int keyCode) {
+    	screen->keyReleaseEvent(keyCode);
+		if (keyCode == MAK_SOFTRIGHT) {
+			closeEvent(); // calls close();
+		}
     }
 };
 
